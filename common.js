feafb13a08fe258e3939c1478eff3410ed8f5468
Indar45 <35995250+Indar45@users.noreply.github.com>
2018-03-07 11:56:15 +0530

    delete old files

--- common.js
+++
@@ -1,9 +0,0 @@
-(function(){
-
-	var winHeight = $(window).height();
-	var winWidth = $(window).width();
-
-	$('body holder').css({'width':winWidth });
-	$('.container').css({'width': winWidth + 38 , 'height' : winHeight });
-
-})();
